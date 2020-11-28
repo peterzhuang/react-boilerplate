b295eff2d6d224d9dafadaf53016e5d04e5fd677
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2020-11-27 23:06:56 -0500

    L22 Async load bundle through code-splitting and react suspense

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
+// import Warning from './Warning'
+const Warning = React.lazy(() => import('./Warning'))
 
 class App extends React.Component {
     state = {
         count: 0
     }
+
+    increment = () => {
+        this.setState(state => ({count: state.count + 1}))
+      }
+    
+    decrement = () => {
+    this.setState(state => ({count: state.count - 1}))
+    }
+
     render() {
         const { count } = this.state
         return (
@@ -13,8 +24,13 @@ class App extends React.Component {
             <h2 className={count > 10 ? 'warning' : null}>
                 Count: {count}
             </h2>
-            <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
-            <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
+            <button onClick={this.increment}>+</button>
+            <button onClick={this.decrement}>-</button>
+            {count > 10 ? 
+                <React.Suspense fallback={null}>
+                    <Warning /> 
+                </React.Suspense>
+            : null}
         </div>
     )
     }
